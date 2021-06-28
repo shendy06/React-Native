d8750337696f7bfe5dfac2ec34b0d0c8e3557770
Shendy <Shendy@Esthers-MacBook-Pro.local>
2021-06-27 20:54:09 -0500

    Swipe and Delete Favorites

--- redux/favorites.js
+++ redux/favorites.js
@@ -6,7 +6,11 @@ export const favorites = (state = [], action) => {
             if (state.includes(action.payload)) {
                 return state;
             }
+            
             return state.concat(action.payload);
+
+        case ActionTypes.DELETE_FAVORITE:
+            return state.filter(favorite => favorite !== action.payload);   
 
         default:
             return state;
